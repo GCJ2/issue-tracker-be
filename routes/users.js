9fcb23933fe8472ea35873b0bab27f904665f0c8
Greg Johnston <gcjohnston91@gmail.com>
2020-07-22 12:04:09 -0400

    Changed user_name seeds toUpper and added toUpper on /users/user/:username. Will update with custom query later

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,50 @@ router.get('/', (req, res) => {
     .then(users =>
       res.status(200).json(users)
     ).catch(error => {
-      res.status(500).json(error)
+    res.status(500).json(error)
   })
+});
+
+router.get('/id/:id', (req, res) => {
+  const {id} = req.params;
+  Users.findByID(id)
+    .then(user => {
+        if (user) {
+          res.status(200).json(user)
+        }
+        else {
+          res.status(404).json({message: 'User not found'})
+        }
+      }
+    ).catch(error => {
+    res.status(500).json(error)
+  })
+});
+
+router.get('/user/:username', (req, res) => {
+  const {username} = req.params;
+  console.log(username)
+  Users.findByUserName(username.toUpperCase())
+    .then(user => {
+        if (user) {
+          res.status(200).json(user)
+        } else {
+          res.status(404).json({message: 'User not found'})
+        }
+      }
+    ).catch(error => {
+    res.status(500).json(error)
+  })
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  Users.add(req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(error =>
+      res.status(500).json({message: error}))
 });
 
 module.exports = router;
--- seeds/users.js
+++ seeds/users.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, user_name: 'GCJ2', password: 'hashedPassword1', first_name: 'Greg', last_name: 'Johnston', role: 1},
-        {id: 2, user_name: 'CodyyLee', password: 'hashedPassword2', first_name: 'Cody', last_name: 'Lee', role: 2},
-        {id: 3, user_name: 'Reececap', password: 'hashedPassword3', first_name: 'Reece', last_name: 'Pierson', role: 3},
-        {id: 4, user_name: 'QuieroPizza', password: 'hashedPassword4', first_name: 'Gabe', last_name: 'Anguiano', role: 4},
+        {id: 1, user_name: 'GCJ2'.toUpperCase(), password: 'hashedPassword1', first_name: 'Greg', last_name: 'Johnston', role: 1},
+        {id: 2, user_name: 'CodyyLee'.toUpperCase(), password: 'hashedPassword2', first_name: 'Cody', last_name: 'Lee', role: 2},
+        {id: 3, user_name: 'Reececap'.toUpperCase(), password: 'hashedPassword3', first_name: 'Reece', last_name: 'Pierson', role: 3},
+        {id: 4, user_name: 'QuieroPizza'.toUpperCase(), password: 'hashedPassword4', first_name: 'Gabe', last_name: 'Anguiano', role: 4},
       ]);
     });
 };
