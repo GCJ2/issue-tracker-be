637b47946aba4d29bc3096355966b904e1420f8b
Greg Johnston <gcjohnston91@gmail.com>
2020-07-30 13:00:41 -0400

    Renamed unportected routes

--- api/server.js
+++ api/server.js
@@ -22,9 +22,9 @@ server.use('/users', logger, restricted, userRouter);
 server.use('/issues', logger, restricted, issueRouter);
 server.use('/comments', logger, restricted, commentsRouter);
 
-// server.use('/users', logger, userRouter);
-// server.use('/issues', logger, issueRouter);
-// server.use('/comments', logger, commentsRouter);
+server.use('/uup', logger, userRouter);
+server.use('/iup', logger, issueRouter);
+server.use('/cup', logger, commentsRouter);
 
 
 server.use((req, res) => {
