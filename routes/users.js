640f5dde456a8fb9e37ad4d18fd2f3b2087ecd8e
Greg Johnston <gcjohnston91@gmail.com>
2020-08-13 14:28:08 -0400

    Admin account set up. Reactivating auth

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,45 @@
-const express = require('express');
-const Users = require('../models/Users');
-const Issues = require('../models/Issues')
+const express = require("express");
+const Users = require("../models/Users");
+const Issues = require("../models/Issues");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Users.getAllUsers()
-    .then(users =>
-      res.status(200).json(users)
-    ).catch(error => {
-    res.status(500).json(error)
-  })
+    .then((users) => res.status(200).json(users))
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await Users.findByID(id);
     if (user) {
       user.issues = await Issues.getIssuesByUserID(id);
       delete user.password;
-      res.status(200).json(user)
+      res.status(200).json(user);
     } else {
-      res.status(404).json({message: 'Could not find user'})
+      res.status(404).json({ message: "Could not find user" });
     }
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 });
 
-
-router.get('/username/:username', async (req, res) => {
+router.get("/username/:username", async (req, res) => {
   try {
-    const {username} = req.params;
+    const { username } = req.params;
     const user = await Users.findByUserName(username);
     if (user) {
       user.issues = await Issues.getIssuesByUserID(user.id);
       delete user.password;
-      res.status(200).json(user)
+      res.status(200).json(user);
     } else {
-      res.status(404).json({message: 'Could not find user'})
+      res.status(404).json({ message: "Could not find user" });
     }
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 });
 
@@ -53,31 +51,32 @@ router.get('/username/:username', async (req, res) => {
 //     res.status(500).json(error))
 // });
 
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
   Users.deleteUser(id)
-    .then(count => {
+    .then((count) => {
       if (count > 0) {
-        res.status(200).json({message: 'User deleted'})
+        res.status(200).json({ message: "User deleted" });
       } else {
-        res.status(404).json({message: `User with ID of ${id} not found`})
+        res.status(404).json({ message: `User with ID of ${id} not found` });
       }
-    }).catch(error =>
-    res.status(500).json(error))
+    })
+    .catch((error) => res.status(500).json(error));
 });
 
-router.patch('/:id', (req, res) => {
-  const {id} = req.params;
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
   const changes = req.body;
   Users.updateUser(id, changes)
-    .then(user => {
+    .then((user) => {
       if (user) {
-        res.status(200).json(user)
+        delete user.password;
+        res.status(200).json(user);
       } else {
-        res.status(404).json({message: "User not found"})
+        res.status(404).json({ message: "User not found" });
       }
-    }).catch(error =>
-    res.status(500).json(error))
+    })
+    .catch((error) => res.status(500).json(error));
 });
 
 module.exports = router;
